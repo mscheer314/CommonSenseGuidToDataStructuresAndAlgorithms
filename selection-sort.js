69fb32feab6ed61d1f106567d62483f25e6a558f
Mark <mark@home-comp.attlocal.net>
2023-04-29 21:47:32 -0500

    rename file

---
+++ selection-sort.js
@@ -0,0 +1,20 @@
+function selectionSort(array) {
+	for (let i = 0; i < array.length - 1; i++) {
+		let lowestNumberIndex = i;
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[j] < array[lowestNumberIndex]) {
+				lowestNumberIndex = j;
+			}
+		}
+		if (lowestNumberIndex != i) {
+			let temp = array[i];
+			array[i] = array[lowestNumberIndex];
+			array[lowestNumberIndex] = temp;
+		}
+	}
+	return array;
+
+}
+
+const result = selectionSort([7,3,4,6,34,2,7,2,1]);
+console.log(result);
